3038825c694a6698a042d07850d0cf9688208808
carlodizon03 <carlodizon03@gmail.com>
2022-11-07 23:28:54 +0800

    added utility func for getting authority info

--- src/interfaces/account.interface.tsx
+++ src/interfaces/account.interface.tsx
@@ -1,6 +1,18 @@
-import { LocalAccount } from "./local-account.interface";
+import * as Hive from '@hiveio/dhive';
 
-export interface Accounts {
-  list: LocalAccount[];
-  hash?: string;
+export type KeyAuth = {
+  key: string  | Hive.PublicKey;
+  weightThreshold: number | null;
+}
+
+export type Keys = {
+    owner?:  KeyAuth[];
+    active?:  KeyAuth[];
+    posting?: KeyAuth[];
+    memo: string;
+}
+
+export type Account = {
+    name: string;
+    keys: Keys;
 }
--- src/utils/hive.utils.tsx
+++ src/utils/hive.utils.tsx
@@ -1,15 +1,58 @@
 import { Client } from "@hiveio/dhive";
-import { LocalAccount } from "../interfaces/local-account.interface";
-
+import * as Hive from '@hiveio/dhive';
+import { KeyAuth, Keys } from "../interfaces/account.interface";
 
 const client = new Client(["https://api.hive.blog", "https://api.hivekings.com", "https://anyx.io", "https://api.openhive.network"]);
 
 const getAccount = async(username: string) => {
     return client.database.getAccounts([username]);
- }
+}
 
- const AccountUtils = {
-    getAccount
- }
+const getAccountAuthorities = async(username: string) => {
+   const account = await getAccount(username);
+   if(account.length === 0){
+      //TODO: return an error here
+      return null;
+   }
+   const ownerAuth: KeyAuth[]|KeyAuth = _getAuthorityInfo(account[0].owner)
+   const activeAuth: KeyAuth[]|KeyAuth = _getAuthorityInfo(account[0].active)
+   const postingAuth: KeyAuth[]|KeyAuth = _getAuthorityInfo(account[0].posting)
+  
+   const keys: Keys = {
+      owner: ownerAuth,
+      active: activeAuth,
+      posting: postingAuth,   
+      memo: account[0].memo_key
+   }
+   return keys;
+}
+
+const _getAuthorityInfo = (auth: Hive.Authority) => {
+   
+   const authoriyInfos: KeyAuth[] = [] ;
+  
+   auth.account_auths.forEach((account) => {
+      const auth: KeyAuth = {
+         key: account[0],
+         weightThreshold: account[1]
+      } 
+      authoriyInfos.push(auth)
+   })
+
+   auth.key_auths.forEach((key) => {
+      const auth: KeyAuth = {
+         key: key[0],
+         weightThreshold: key[1]
+      }
+      authoriyInfos.push(auth)
+   })
+   
+   return authoriyInfos
+}
+
+const AccountUtils = {
+    getAccount,
+    getAccountAuthorities
+}
 
  export default AccountUtils;
