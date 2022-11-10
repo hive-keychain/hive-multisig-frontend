f587a50453f1f66621dd7c4bde0df84460961c40
carlodizon03 <carlodizon03@gmail.com>
2022-11-11 01:45:10 +0800

    converted key to string

--- src/components/AuthorityList.tsx
+++ src/components/AuthorityList.tsx
@@ -2,40 +2,43 @@ import React, { Component } from "react";
 import ListGroup from 'react-bootstrap/ListGroup';
 import {Keys} from '../interfaces/account.interface';
 
+interface Iprops {
+    authkey: Keys;
+}
 
-class AuthorityList extends Component<Keys>{
-    constructor(props:Keys){
+class AuthorityList extends Component<Iprops>{
+    constructor(props:Iprops){
         super(props);
-        console.log(props);
     }
     render(){
         return(
             <div>
+                <h2 >Owner</h2>
                 <div>
                     <ListGroup variant="flush">
                         { 
-                            this.props.owner.map((k, w) =>
-                                    <ListGroup.Item>k</ListGroup.Item>
+                            this.props.authkey.owner.map((k, w): React.ReactNode =>
+                                    <ListGroup.Item>{k.key.toString()}</ListGroup.Item>
                                 )
                         }
                     </ListGroup>
                 </div>
-            
+                <h2>Active</h2>
                 <div>
                     <ListGroup variant="flush">
                         { 
-                            this.props.active.map((k, w) =>
-                                    <ListGroup.Item>k</ListGroup.Item>
+                            this.props.authkey.active.map((k, w) =>
+                                    <ListGroup.Item>{k.key.toString()}</ListGroup.Item>
                                 )
                         }
                     </ListGroup>
                 </div>
-            
+                <h2>Posting</h2>
                 <div>
                     <ListGroup variant="flush">
                         { 
-                            this.props.posting.map((k, w) =>
-                                    <ListGroup.Item>k</ListGroup.Item>
+                            this.props.authkey.posting.map((k, w) =>
+                                    <ListGroup.Item>{k.key.toString()}</ListGroup.Item>
                                 )
                         }
                     </ListGroup>
