47172b81258c6441da308db0ee5fbc7440d0c0c5
carlodizon03 <carlodizon03@gmail.com>
2022-11-11 00:07:01 +0800

    added authority list component

--- src/components/AuthorityList.tsx
+++ src/components/AuthorityList.tsx
@@ -1,35 +1,47 @@
+import React, { Component } from "react";
 import ListGroup from 'react-bootstrap/ListGroup';
 import {Keys} from '../interfaces/account.interface';
 
-export default (keys: Keys) => (
-    <ListGroup variant="flush">
-        <ListGroup variant="flush">
-            <ListGroup.Item>
-                {
-                    keys.owner.map((account) => (
-                        <ListGroup.Item>account.key</ListGroup.Item>
-                    ))
-                }
-            </ListGroup.Item>
-        </ListGroup>
-        <ListGroup variant="flush">
-            <ListGroup.Item>
-                {
-                    keys.active.map((account) => (
-                        <ListGroup.Item>account.key</ListGroup.Item>
-                    ))
-                }
-            </ListGroup.Item>
-        </ListGroup>
-        <ListGroup variant="flush">
-            <ListGroup.Item>
-                {
-                    keys.posting.map((account) => (
-                        <ListGroup.Item>account.key</ListGroup.Item>
-                    ))
-                }
-            </ListGroup.Item>
-        </ListGroup>
-    </ListGroup>
 
-);
+class AuthorityList extends Component<Keys>{
+    constructor(props:Keys){
+        super(props);
+        console.log(props);
+    }
+    render(){
+        return(
+            <div>
+                <div>
+                    <ListGroup variant="flush">
+                        { 
+                            this.props.owner.map((k, w) =>
+                                    <ListGroup.Item>k</ListGroup.Item>
+                                )
+                        }
+                    </ListGroup>
+                </div>
+            
+                <div>
+                    <ListGroup variant="flush">
+                        { 
+                            this.props.active.map((k, w) =>
+                                    <ListGroup.Item>k</ListGroup.Item>
+                                )
+                        }
+                    </ListGroup>
+                </div>
+            
+                <div>
+                    <ListGroup variant="flush">
+                        { 
+                            this.props.posting.map((k, w) =>
+                                    <ListGroup.Item>k</ListGroup.Item>
+                                )
+                        }
+                    </ListGroup>
+                </div>
+            </div>
+        )
+    }
+}
+export default AuthorityList;
