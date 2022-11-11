e741870c6a5e0b051644ab3cbdace4f5cc438da6
carlodizon03 <carlodizon03@gmail.com>
2022-11-11 14:38:39 +0800

    cleanup

--- src/components/AuthorityList.tsx
+++ src/components/AuthorityList.tsx
@@ -1,36 +1,35 @@
 import React, { Component } from "react";
 import { Badge } from "react-bootstrap";
 import ListGroup from 'react-bootstrap/ListGroup';
-import {Authority, Keys} from '../interfaces/account.interface';
+import {Authority, KeyAuth, Keys} from '../interfaces/account.interface';
 
 interface Iprops {
     authkey: Keys;
 }
 
+
 class AuthorityList extends Component<Iprops>{
     constructor(props:Iprops){
         super(props);
     }
-    render(){
-            return(
-            
-                <ListGroup as="ol">            
+
+    addList = (name:string, authority:KeyAuth[])=>{
+        return (
                     <ListGroup.Item
                         as="li"
                         className="d-flex"
                     >
                         <div className="ms-2 text-start">
-                            <div className="fw-bold">Owner</div>
+                            <div className="fw-bold">{name}</div>
                             <div className="ms-2 me-auto">
                                 { 
-                                    this.props.authkey.owner.length>0?
+                                    authority.length>0?
                                     <ListGroup variant="flush">
                                         {   
-                                            this.props.authkey.owner.map((k):  React.ReactNode=>
-                                            <ListGroup.Item key={k.key.toString()} className='d-flex justify-content-between align-items-center'>
+                                            authority.map((k):  React.ReactNode=>
+                                            <ListGroup.Item key={k.key.toString()}>
                                                 <div>
                                                     {k.key.toString()}
-                                      
                                                     <Badge bg="secondary" pill >{k.weight}</Badge>
                                                 </div>
                                             </ListGroup.Item>
@@ -42,59 +41,18 @@ class AuthorityList extends Component<Iprops>{
                             </div>
                         </div>
                     </ListGroup.Item>
-    
-                    <ListGroup.Item
-                        as="li"
-                        className="d-flex"
-                    >
-    
-                        <div className="ms-2 text-start">
-                            <div className="fw-bold">Active</div>
-                            <div className="ms-2 me-auto">
-                                {
-                                    this.props.authkey.active.length>0?
-                                    <ListGroup variant="flush">
-                                        { 
-                                            this.props.authkey.active.map((k):  React.ReactNode=>
-                                                        <ListGroup.Item key={k.key.toString()} className='d-flex justify-content-between align-items-center'>
-                                                            {k.key.toString()}
-                                                            <Badge bg="secondary" pill className="float-left">{k.weight}</Badge>
-                                                        </ListGroup.Item>
-                                                )
-                                        }
-                                    </ListGroup>
-                                    :''
-                                }
-                            </div>
-                                    
-                        </div>
-                    </ListGroup.Item>
-    
-                    <ListGroup.Item
-                        as="li"
-                        className="d-flex"
-                    >
-    
-                        <div className="ms-2 text-start">
-                            <div className="fw-bold">Posting</div>
-                            <div className="ms-2 me-auto">
-                                {
-                                    this.props.authkey.posting.length>0?
-                                    <ListGroup variant="flush">
-                                        { 
-                                            this.props.authkey.posting.map((k):  React.ReactNode=>
-                                                        <ListGroup.Item key={k.key.toString()} className='d-flex justify-content-between align-items-center'>
-                                                            {k.key.toString()}
-                                                            <Badge bg="secondary" pill className="float-left">{k.weight.toString()}</Badge>
-                                                        </ListGroup.Item>
-                                                )
-                                        }
-                                    </ListGroup>
-                                    :''
-                                }
-                            </div>
-                        </div>
-                    </ListGroup.Item>
+        )
+    }
+    render(){
+            return(
+            
+                <ListGroup as="ol">            
+                    <div>
+                        {this.addList('Owner',this.props.authkey.owner)}
+                        {this.addList('Active',this.props.authkey.active)}
+                        {this.addList('Posting',this.props.authkey.posting)}    
+                    </div>
+                    
                 </ListGroup>
     
             )
