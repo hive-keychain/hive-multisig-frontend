1037ebee951f3286f87480f29d0cd42b80e60ef0
carlodizon03 <carlodizon03@gmail.com>
2022-11-11 15:30:43 +0800

    added invalid username text

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,6 +5,7 @@ import Button from 'react-bootstrap/Button';
 
 interface IProps {
     handleClick: (username: string) => void;
+    isValidUser: boolean;
 }
 interface IState{
     username: string;
@@ -13,27 +14,32 @@ interface IState{
 class SearchBar extends Component<IProps, IState>{
     constructor(props:IProps){
         super(props);
-        this.state = { username: ''};
+        this.state = {
+             username: ''
+            };
     }
    
     render() {
         return (
-            <InputGroup className="mb-3">
-            <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-            <Form.Control
-            placeholder="Username"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-            onChange={e=>this.setState({username: e.target.value})}
-            />
-            <Button 
-                variant="outline-secondary" 
-                id="button-addon2"
-                onClick={(event) => this.props.handleClick(this.state.username)}
-            >
-                Search
-            </Button>
-        </InputGroup>
+            <div>
+                <div className="ms-2 text-start" style={{color:"red"}}>{this.props.isValidUser?'':'Invalid User'}</div>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                    <Form.Control
+                    placeholder="Username"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    onChange={e=>this.setState({username: e.target.value})}
+                    />
+                    <Button 
+                        variant="outline-secondary" 
+                        id="button-addon2"
+                        onClick={(event) => this.props.handleClick(this.state.username)}
+                    >
+                        Search
+                        </Button>
+                </InputGroup>
+            </div>
         )
     }
 
