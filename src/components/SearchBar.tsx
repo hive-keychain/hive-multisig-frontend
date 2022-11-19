39720f1fca7038d2c843035d095a22dbf85014db
carlodizon03 <carlodizon03@gmail.com>
2022-11-19 18:14:21 +0800

    refactore to functional componnet

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,49 +1,29 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 
-interface IProps {
-    handleClick: (username: string) => void;
-    isValidUser: boolean;
-}
-interface IState{
-    username: string;
-}
+const SearchBar = (props:any) =>{
+    const [input,setInput] = useState<string>();
+    return(
 
-class SearchBar extends Component<IProps, IState>{
-    constructor(props:IProps){
-        super(props);
-        this.state = {
-             username: ''
-            };
-    }
-   
-    render() {
-        return (
-            <div>
-                <div className="ms-2 text-start" style={{color:"red"}}>{this.props.isValidUser?'':'Invalid User'}</div>
-                <InputGroup className="mb-3">
-                    <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-                    <Form.Control
-                    placeholder="Username"
-                    aria-label="Username"
-                    aria-describedby="basic-addon1"
-                    onChange={e=>this.setState({username: e.target.value})}
-                    />
-                    <Button 
-                        variant="outline-secondary" 
-                        id="button-addon2"
-                        onClick={(event) => this.props.handleClick(this.state.username)}
-                    >
-                        Search
-                        </Button>
-                </InputGroup>
-            </div>
-        )
-    }
-
-}
-
+      <div>
+        <div className="ms-2 text-start" style={{color:"red"}}>{props.isValidUserName?'':'Invalid User'}</div>
+        <InputGroup className='mb-3'>
+          <Form.Control
+            placeholder={input?input:"Username"}
+            aria-label="Username"
+            aria-describedby="basic-addon2"  
+            onChange={(e)=>setInput(e.target.value)}
+          />
+          <Button 
+            variant='outline-secondary' 
+            id='button-addon2'
+            onClick= {()=>props.handleOnSearchSubmit(input)}
+          >Search</Button>
+        </InputGroup>
+      </div>
+    )
+  }
 
 export default SearchBar;
