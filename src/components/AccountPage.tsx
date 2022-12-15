f95aadcd1b9b98a225c79d9b1f86c40ada5dcfff
carlodizon03 <carlodizon03@gmail.com>
2022-12-16 00:48:29 +0800

    WIP: local update redux

--- src/components/AccountPage.tsx
+++ src/components/AccountPage.tsx
@@ -1,24 +1,41 @@
-import { Stack } from 'react-bootstrap';
+import { Button, Stack } from 'react-bootstrap';
 import { Authorities } from '../interfaces';
+import { useAppDispatch } from '../redux/app/hooks';
+import { initializeAuthorities } from '../redux/features/updateAuthorities/updateAuthoritiesSlice';
 import { AuthorityCard } from './cards/AuthorityCard';
 
-interface IAccountPageProp{
-  authorities:Authorities
+interface IAccountPageProp {
+  authorities: Authorities;
 }
-function AccountPage({authorities}:IAccountPageProp) {
-  return (
-          authorities?
-          <div>
-          <Stack gap={3}>
-            <AuthorityCard authorityName={'Owner'} authority={authorities.owner}/>
-            <AuthorityCard authorityName={'Active'} authority={authorities.active}/>
-            <AuthorityCard authorityName={'Posting'} authority={authorities.posting}/>
-          </Stack>
-        </div>
-        :<div></div>
-      );
+function AccountPage({ authorities }: IAccountPageProp) {
+  const dispatch = useAppDispatch();
+
+  if (authorities) {
+    dispatch(initializeAuthorities(authorities));
   }
+  return authorities ? (
+    <div>
+      <Stack gap={3}>
+        <AuthorityCard authorityName={'Owner'} authority={authorities.owner} />
+        <AuthorityCard
+          authorityName={'Active'}
+          authority={authorities.active}
+        />
+        <AuthorityCard
+          authorityName={'Posting'}
+          authority={authorities.posting}
+        />
+        <Stack direction="horizontal" gap={2}>
+          <Button className="ms-auto" variant="secondary">
+            Reset
+          </Button>{' '}
+          <Button variant="success">Update</Button>{' '}
+        </Stack>
+      </Stack>
+    </div>
+  ) : (
+    <div></div>
+  );
+}
 
-
-
-export default AccountPage
+export default AccountPage;
--- src/redux/features/updateAuthorities/updateAuthoritiesSlice.ts
+++ src/redux/features/updateAuthorities/updateAuthoritiesSlice.ts
@@ -1,103 +1,120 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { IAccountKeysCardProps } from "../../../components/cards/AuthorityCard"
-import { Authorities, BroadCastResponseType, IDHiveAccountUpdateBroadcast, IHiveAccountUpdateBroadcast } from "../../../interfaces"
-import { AccountUpdateBroadcast } from "../../../utils/hive-keychain.utils"
-import { BroadcastUpdateAccount } from "../../../utils/hive.utils"
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import * as _ from 'lodash';
+import { IAccountKeysCardProps } from '../../../components/cards/AuthorityCard';
+import {
+  Authorities,
+  BroadCastResponseType,
+  IDHiveAccountUpdateBroadcast,
+  IHiveAccountUpdateBroadcast,
+} from '../../../interfaces';
+import { AccountUpdateBroadcast } from '../../../utils/hive-keychain.utils';
+import { BroadcastUpdateAccount } from '../../../utils/hive.utils';
+export type AuthorityUpdateStateType = {
+  Authorities: Authorities;
+  NewAuthorities: Authorities;
+  isUpdateSucces: boolean;
+  updateCount: number;
+  isOwnerAuthUpdated: boolean;
+  ownerAuthUpdateCount: number;
+  error: string;
+};
 
-export type AuthorityStateType = {
-    Authorities: Authorities
-    isUpdateSucces: boolean,
-    error: string
-}
-
-const initialState: AuthorityStateType = {
-    Authorities: null,
-    isUpdateSucces: false,
-    error: ''
-}
-
+const initialState: AuthorityUpdateStateType = {
+  Authorities: null,
+  NewAuthorities: null,
+  isUpdateSucces: false,
+  updateCount: 0,
+  isOwnerAuthUpdated: false,
+  ownerAuthUpdateCount: 0,
+  error: '',
+};
 
 export const hiveKeyChainRequestBroadCast = createAsyncThunk(
-    'updateAuthority/hiveBroadcast',
-    async(props:IHiveAccountUpdateBroadcast) => {
-        const response = await AccountUpdateBroadcast(props)
-        return response;
-    }
-)
+  'updateAuthority/hiveBroadcast',
+  async (props: IHiveAccountUpdateBroadcast) => {
+    const response = await AccountUpdateBroadcast(props);
+    return response;
+  },
+);
 
 export const dhiveBroadcastUpdateAccount = createAsyncThunk(
   'updateAuthority/dhiveBroadcast',
-  async(props:IDHiveAccountUpdateBroadcast) => {
-    const response = await BroadcastUpdateAccount(props)
+  async (props: IDHiveAccountUpdateBroadcast) => {
+    const response = await BroadcastUpdateAccount(props);
     return response;
-  }
-)
+  },
+);
 
 const updateAuthoritySlice = createSlice({
-    name: 'updateAuthority',
-    initialState,
-    reducers: {
-      initializeAuthorities(state, action: PayloadAction<Authorities>){
-        state.Authorities = action.payload
-      },
-      updateAccount(state, action: PayloadAction<IAccountKeysCardProps> ){
-        switch(action.payload.authorityName.toLowerCase()){
-          case "owner":
-            action.payload.authAccountType.toLowerCase() === 'account'?
-            state.Authorities.owner.account_auths = action.payload.accountKeyAuths
-            :state.Authorities.owner.key_auths = action.payload.accountKeyAuths
-            break;
-          case "active":
-            action.payload.authAccountType.toLowerCase() === 'active'?
-            state.Authorities.active.account_auths = action.payload.accountKeyAuths
-            :state.Authorities.owner.key_auths = action.payload.accountKeyAuths
-            break;
-          case "posting":
-            action.payload.authAccountType.toLocaleLowerCase() ==='posting'?
-            state.Authorities.posting.account_auths = action.payload.accountKeyAuths
-            :state.Authorities.posting.account_auths = action.payload.accountKeyAuths
-            break;
-        }
-      }
-
+  name: 'updateAuthority',
+  initialState,
+  reducers: {
+    initializeAuthorities(state, action: PayloadAction<Authorities>) {
+      state.Authorities = action.payload;
+      state.NewAuthorities = action.payload;
     },
-    extraReducers: (builder) => {
-        builder.addCase(
-            hiveKeyChainRequestBroadCast.fulfilled,
-            (state, action: PayloadAction<BroadCastResponseType>) => {
-              if (action.payload.success) {
-                state.Authorities =  null;
-                state.isUpdateSucces = true;
-                state.error = '';
-              }
-            },
-          );
-          builder.addCase(
-            hiveKeyChainRequestBroadCast.rejected,
-            (state, action) => {
-                state.Authorities = null;
-                state.isUpdateSucces = false;
-                state.error = 'Hive Keychain Update Error';
-            },
-          );
-          builder.addCase(
-            dhiveBroadcastUpdateAccount.fulfilled,
-            (state, action: PayloadAction<any>) => {
-              state.Authorities = null;
-              state.isUpdateSucces = true;
-              state.error = '';
-            }
-          );
-          builder.addCase(
-            dhiveBroadcastUpdateAccount.rejected,
-            (state, action) => {
-              state.Authorities = null;
-                state.isUpdateSucces = false;
-                state.error = 'DHive Update Error';
-            }
-          )
-    }
-})
+    updateAccount(state, action: PayloadAction<IAccountKeysCardProps>) {
+      switch (action.payload.authorityName.toLowerCase()) {
+        case 'owner':
+          action.payload.authAccountType.toLowerCase() === 'accounts'
+            ? (state.NewAuthorities.owner.account_auths =
+                action.payload.accountKeyAuths)
+            : (state.NewAuthorities.owner.key_auths =
+                action.payload.accountKeyAuths);
+          break;
+        case 'active':
+          action.payload.authAccountType.toLowerCase() === 'accounts'
+            ? (state.NewAuthorities.active.account_auths =
+                action.payload.accountKeyAuths)
+            : (state.NewAuthorities.active.key_auths =
+                action.payload.accountKeyAuths);
+          break;
+        case 'posting':
+          action.payload.authAccountType.toLowerCase() === 'accounts'
+            ? (state.NewAuthorities.posting.account_auths =
+                action.payload.accountKeyAuths)
+            : (state.NewAuthorities.posting.key_auths =
+                action.payload.accountKeyAuths);
+          break;
+      }
+      state.isOwnerAuthUpdated = !_.isEqual(
+        state.NewAuthorities.owner,
+        state.Authorities.owner,
+      );
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(
+      hiveKeyChainRequestBroadCast.fulfilled,
+      (state, action: PayloadAction<BroadCastResponseType>) => {
+        if (action.payload.success) {
+          state.Authorities = null;
+          state.isUpdateSucces = true;
+          state.error = '';
+        }
+      },
+    );
+    builder.addCase(hiveKeyChainRequestBroadCast.rejected, (state, action) => {
+      state.Authorities = null;
+      state.isUpdateSucces = false;
+      state.error = 'Hive Keychain Update Error';
+    });
+    builder.addCase(
+      dhiveBroadcastUpdateAccount.fulfilled,
+      (state, action: PayloadAction<any>) => {
+        state.Authorities = null;
+        state.isUpdateSucces = true;
+        state.error = '';
+      },
+    );
+    builder.addCase(dhiveBroadcastUpdateAccount.rejected, (state, action) => {
+      state.Authorities = null;
+      state.isUpdateSucces = false;
+      state.error = 'DHive Update Error';
+    });
+  },
+});
 
 export default updateAuthoritySlice.reducer;
-export const {initializeAuthorities, updateAccount} = updateAuthoritySlice.actions
+export const { initializeAuthorities, updateAccount } =
+  updateAuthoritySlice.actions;
