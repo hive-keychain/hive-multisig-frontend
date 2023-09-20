69f221370282491cf2fb70035ad0e7c7d20320e8
stoodkev <quentin.correa@gmail.com>
2023-09-20 11:37:26 +0800

    use hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import './index.css';
