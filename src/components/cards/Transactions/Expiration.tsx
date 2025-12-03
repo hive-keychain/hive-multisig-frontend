b7a2276df6c6a209abcb672283c67f5d88909d6f
stoodkev <quentin.correa@gmail.com>
2025-12-03 17:22:23 +0800

    hardcode max hours

--- src/components/cards/Transactions/Expiration.tsx
+++ src/components/cards/Transactions/Expiration.tsx
@@ -1,5 +1,3 @@
-import { Client } from '@hiveio/dhive';
-import { HiveUtils } from 'hive-multisig-sdk/src/utils/hive.utils';
 import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { Col, FloatingLabel, Form, Row } from 'react-bootstrap';
 import { IExpiration } from '../../../interfaces/transaction.interface';
@@ -10,40 +8,8 @@ interface IExpirationProp {
 export const Expiration = ({ setExpiration }: IExpirationProp) => {
   const [minutes, setMinutes] = useState<number>(10);
   const [hours, setHours] = useState<number>(0);
-  const [supportsAboveOneHour, setSupportsAboveOneHour] =
-    useState<boolean>(false);
+  const supportsAboveOneHour = true;
 
-  useEffect(() => {
-    console.log('hi');
-    HiveUtils.getCurrentVersion().then((version) => {
-      console.log('versions', version);
-    });
-  }, []);
-  useEffect(() => {
-    // Use dhive to query condenser.get_hardfork_version and gate >1h options by HF >= 28
-    const client = new Client([
-      'https://api.hive.blog',
-      'https://api.hivekings.com',
-      'https://api.deathwing.me',
-      'https://anyx.io',
-      'https://api.openhive.network',
-    ]);
-    client
-      .call('condenser_api', 'get_hardfork_version', [])
-      .then((version: string) => {
-        const parts = String(version).split('.');
-        const hf = parseInt(parts[1] || '0', 10);
-        const isSupported = hf >= 28;
-        setSupportsAboveOneHour(isSupported);
-        if (!isSupported && hours > 1) {
-          setHours(1);
-        }
-      })
-      .catch(() => {
-        setSupportsAboveOneHour(false);
-        if (hours > 1) setHours(1);
-      });
-  }, []);
   useEffect(() => {
     if (hours === 24 && minutes !== 0) {
       setMinutes(0);
